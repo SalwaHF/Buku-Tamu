efefe3d565ca0b837f96fe037affbddae702d74b
Salwa Husnun <salwahusnun6@gmail.com>
2018-11-28 16:31:07 +0700

    oke

---
+++ Bukutamu/coba_db.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2018 at 10:26 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coba_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_user` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_user`, `password`) VALUES
+('fauziah', '123'),
+('husnun', 'husnun'),
+('salwa', 'salwa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku_tamu`
+--
+
+CREATE TABLE `buku_tamu` (
+  `id_bktamu` int(10) NOT NULL,
+  `status_bktamu` varchar(30) DEFAULT NULL,
+  `nm_bktamu` varchar(30) NOT NULL,
+  `email_bktamu` varchar(30) DEFAULT NULL,
+  `alamat_bktamu` varchar(150) NOT NULL,
+  `tgl_bktamu` varchar(30) NOT NULL,
+  `komentar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `buku_tamu`
+--
+
+INSERT INTO `buku_tamu` (`id_bktamu`, `status_bktamu`, `nm_bktamu`, `email_bktamu`, `alamat_bktamu`, `tgl_bktamu`, `komentar`) VALUES
+(1, 'kkk', 'kkkk', 'kkk', 'kkk', 'kk', 'kkk'),
+(11, 'aa', 'aa', 'AA', 'aa', '', 'yuioo'),
+(88, 'io', 'kffe', 'uiop', 'wqqq', '', 'plo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galeri`
+--
+
+CREATE TABLE `galeri` (
+  `id_galeri` int(5) NOT NULL,
+  `nm_galeri` varchar(50) NOT NULL,
+  `ket` text NOT NULL,
+  `tgl_galeri` varchar(25) NOT NULL,
+  `gambar` varchar(35) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  ADD PRIMARY KEY (`id_bktamu`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  MODIFY `id_bktamu` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
